35b30b851ff4e0dae9ec78ba2288f359c02806b1
GoldSheep3 <gold_sheep_3@163.com>
2026-03-25 13:57:39 +0800

    fix: 添加帮助命令的响应

--- plugins/daily_partner.py
+++ plugins/daily_partner.py
@@ -275,7 +275,7 @@ class PluginManager:
             return
 
         await anyio.Path(path.parent).mkdir(parents=True, exist_ok=True)
-        dumped_dict = {str(k): getattr(v, "model_dump", v.dict)() for k, v in instance.members.items()}
+        dumped_dict = {str(k): getattr(v, "model_dump", v.model_dump)() for k, v in instance.members.items()}
         dumped_bytes = orjson.dumps(dumped_dict, option=orjson.OPT_INDENT_2)
         await anyio.Path(path).write_bytes(dumped_bytes)
 
@@ -385,13 +385,37 @@ async def get_pool(bot: Bot, group_id: int, user_id: int, config: dict, group_in
     return pool
 
 # --- 响应器 ---
+help = on_regex(r"^(今日老婆|jrlp)\s*(帮助|help)$", priority=5, block=True)
 jrlp = on_regex(r"^(今日老婆|jrlp)$", priority=5, block=True)
 hlp = on_regex(r"^(换老婆|hlp)$", priority=5, block=True)
 hlg = on_regex(r"^(换老公|hlg)$", priority=5, block=True)
-qq = on_regex(r"^(强娶|qq)", priority=5, block=True)
+qq = on_regex(r"^强娶", priority=5, block=True)
 lh = on_regex(r"^(离婚|lh)$", priority=5, block=True)
 jrlg = on_regex(r"^(今日老公|jrlg)$", priority=5, block=True)
 toggle_status = on_regex(r"^(不当老婆|当老婆|不娶bot|娶bot)$", priority=5, block=True)
+
+@help.handle()
+async def _(event: MessageEvent):
+    await help.finish("""
+帮助 | DailyPartner（今日老婆）
+
+1. 今日老婆 / jrlp
+   随机抽选一个「老婆」，每日一次，各群独立
+2. 今日老公 / jrlg
+   根据「今日老婆」的抽选结果，显示当前锚定的「老公」
+3. 换老婆 / hlp
+   重新抽选一个「老婆」（有次数限制）
+4. 换老公 / hlg
+   和当前的「老公」离婚（有次数限制）
+5. 强娶 [QQ 或 at]
+   尝试指定某个群友为你的「老婆」（有次数等限制）
+6. 离婚 / lh
+   和当前的「老婆」离婚（谨慎！）
+7. 当老婆 / 不当老婆
+   配置项：控制你是否可以参与抽选，以及是否可以被抽选
+8. 娶bot / 不娶bot
+   配置项：控制你是否可以娶该群的QQ官方机器人作为「老婆」
+""".strip())
 
 @jrlp.handle()
 async def _(bot: Bot, event: MessageEvent):
